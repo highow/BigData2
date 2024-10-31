93614721ea2457acd3234b8f91cd9cbc877b01c2
Han <gksdnwls10@gmail.com>
2024-10-31 10:28:19 +0900

    BP 2.0

---
+++ week09/week09_web02.py
@@ -0,0 +1,26 @@
+import urllib.request
+from urllib.request import urlopen
+
+import pandas as pd
+from bs4 import BeautifulSoup
+
+shops = []
+
+for i in range(1, 50):
+    url = f'https://www.hollys.co.kr/store/korea/korStore2.do?pageNo={i}&sido=&gugun=&store='
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    tbody = soup.find('tbody')
+    trs = tbody.find_all('tr')
+
+    for tr in trs:
+        tds = tr.find_all('td')
+        shop_name = tds[1].string
+        shop_address = tds[3].string
+        shop_phone = tds[5].string
+
+        shops.append([shop_name]+[shop_address]+[shop_phone])
+
+#print(shops)
+hollys_df = pd.DataFrame(shops, columns=('매장이름', '주소', '전화번호'))
+hollys_df.to_csv('hollys.csv', mode='w')
