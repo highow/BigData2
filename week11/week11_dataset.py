80a3765d28427285aa07701ce95a01aa62b9efaf
Han <gksdnwls10@gmail.com>
2024-11-07 10:51:45 +0900

    v2.8 BP2

--- week11/week11_dataset.py
+++ week11/week11_dataset.py
@@ -3,16 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 titanic = sns.load_dataset('titanic')
+# print(titanic['sex'].head())
+gender_survival = titanic.groupby(by='sex')['survived'].mean()
+print(gender_survival)
+
+
 # titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
 # titanic['deck'].fillna('Unknown', inplace=True)
 # print(titanic['deck'])
 # print(titanic.info())
 
-print(titanic['survived'])
-print(titanic.info())
+# print(titanic['survived'])
+# print(titanic.info())
 
-sns.countplot(data=titanic, x='survived')
-plt.title('Survived (0 = No, 1 = Yes)')
-plt.xlabel('Survived')
-plt.ylabel('Count')
-plt.show()
+# sns.countplot(data=titanic, x='survived')
+# plt.title('Survived (0 = No, 1 = Yes)')
+# plt.xlabel('Survived')
+# plt.ylabel('Count')
+# plt.show()
