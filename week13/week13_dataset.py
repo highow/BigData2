470104683bf2d08d243d261f309d848fb182079c
Han <gksdnwls10@gmail.com>
2024-11-21 11:29:48 +0900

    v3.8 BP2

--- week13/week13_dataset.py
+++ week13/week13_dataset.py
@@ -1,12 +1,16 @@
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 mpg = sns.load_dataset('mpg')
-print(mpg.info())
+mpg.drop(['name'], axis=1, inplace=True)
 mpg.dropna(inplace=True)    # 결측치가 있는 행 제거 (원본 데이터 변경)
+mpg = pd.get_dummies(mpg, columns=['origin'], drop_first=True)
 print(mpg.info())
-print(mpg.head())
-print(mpg.tail())
-print(mpg[['origin']])
-print(mpg.describe())
+
+#독립변수, 의존변수
+X = mpg.drop(['mpg'], axis=1)   # 레이블 컬럼 제거
+y = mpg['mpg']  #레이블 (타겟 변수)
+
+print(X.dtypes) # 각 열의 데이터 타입 확인
